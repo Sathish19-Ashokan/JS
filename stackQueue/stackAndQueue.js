dd155e64a6ccd20349f4b7c8b7566c8b30225d4f
Sathish <sathish.a@v2solutions.com>
2021-05-31 11:26:53 +0530

    final code edited return

--- stackQueue/stackAndQueue.js
+++ stackQueue/stackAndQueue.js
@@ -1,38 +1,22 @@
 function Stack(MaxSize){
-    let func = Object.create(stack);
+    let func = {};
     func.MaxSize = MaxSize;
     func.top = -1;
     func.arr = [];
 
-    return func;
-}
-
-function Queue(MaxSize1){
-    let func = Object.create(queue);
-    func.MaxSize1 = MaxSize1;
-    func.front = -1;
-    func.top = -1;
-    func.arr = [];
-
-    return func;
-}
-
-const stack = {
-    push(element){
+    func.push = function (element){
         this.top++;
         if(this.top >=0 && this.top < this.MaxSize){
             this.arr[this.top] = element;
-            return (`${element} is pushed. In index = ${this.top}`);
          }
         else{
             this.top--;
-            return (`Stack OverFlow. Current Index = ${this.top}`);
+            return "Stack Overflow";
         }
-    },
+    }
 
-    pop(){
+    func.pop = function (){
         if(this.top >=0 && this.top < this.MaxSize){
-            //console.log(this.top);
             let del = this.arr[this.top];
             let temp = [];
             temp = this.arr;
@@ -41,16 +25,24 @@ const stack = {
                 this.arr[i] = temp[i];
             }
             this.top--;
-            return (`Poped element is ${del}. From index = ${this.top + 1}`);
+            return del;
         }
         else{
-            return (`Stack UnderFlow. Current index = ${this.top}`);
+            return "Stack UnderFlow";
         }
     }
+
+    return func;
 }
 
- const queue = {
-    add(element){
+function Queue(MaxSize1){
+    let func = {};
+    func.MaxSize1 = MaxSize1;
+    func.front = -1;
+    func.top = -1;
+    func.arr = [];
+
+    func.add = function (element){
         
         this.top++;
         if(this.top === 0){
@@ -59,32 +51,17 @@ const stack = {
         if(this.top >= 0 && this.top < this.MaxSize1)
         {
             this.arr[this.top] = element;
-            return (`${element} is added. In index = ${this.top}`);
         }
         else{
             this.top--;
-            return (`Queue overflow. Current Index = ${this.top}`);
+            return "Queue overflow";
         }
-    },
+    }
 
-    // remove(){
-    //     if(this.top >= 0 && this.top < this.MaxSize1)
-    //     {
-    //         let pop1 = this.arr[this.front];
-    //         this.front++;
-    //         this.top--;
-    //         //this.arr = this.arr - this.arr[0];
-    //         return (`Removed element = ${pop1}. From index = ${this.front - 1}`);
-    //     }
-    //     else{
-    //         return (`Queue UnderFlow. Current Index = ${this.top}`);
-    //     }
-    // }
-
-    remove(){
+    func.remove = function (){
         if(this.top === 0){
             this.top--;
-            return (`Removed element = ${this.arr[0]}  From index = 0`);
+            return this.arr[0];
         }
         if(this.top > 0 && this.top < this.MaxSize1){
             let del = this.arr[0];
@@ -94,14 +71,13 @@ const stack = {
             for(let i=0; i<this.top ; i++){
                 this.arr[i] = temp[i+1];
             }
-            // for(let i=0 ; i<this.top ; i++){
-            //     this.arr[i] = this.arr[i+1];
-            // }
             this.top--;
-            return (`Removed element = ${del}  From index = 0`);
+            return del;
         }
         else{
-            return (`Queue UnderFlow. Current Index = ${this.top}`);
+            return "Queue UnderFlow";
         }
     }
+
+    return func;
 }
